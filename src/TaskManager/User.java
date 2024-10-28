8686274b16a8031f59ca2c5a356d451f581d4bf3
RodneyCStead <RodCStead@gmail.com>
2024-10-28 09:06:11 -0230

    Added UserLogic List logic to the Class

--- src/TaskManager/User.java
+++ src/TaskManager/User.java
@@ -1,4 +1,28 @@
 package TaskManager;
 
 public class User {
+    private String name;
+    private TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(new Task(description));
+    }
+
+    public void markTaskAsCompleted(String description) {
+        taskList.markTaskAsCompleted(description);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+    }
 }
